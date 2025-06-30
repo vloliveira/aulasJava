57995b4cb3ebdf7a7de2ebc75638710b808f048f
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-06-30 10:30:20 -0300

    exercícios de Herança

---
+++ heranca/src/heranca/Pessoa.java
@@ -0,0 +1,15 @@
+package heranca;
+
+import java.util.Date;
+
+public class Pessoa {
+	public String nome;
+	public String cpf;
+	public String telefone;
+	
+	public Pessoa (String nome, String cpf, String telefone) {
+		this.nome=nome;
+		this.cpf=cpf;
+		this.telefone=telefone;
+	}
+}
